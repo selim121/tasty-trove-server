2d46479189b86f82b3c946e21355a8600106db5c
selim121 <selimkhan.bdkhan@gmail.com>
2023-05-29 19:59:43 +0600

    jwt added

--- index.js
+++ index.js
@@ -9,6 +9,22 @@ const port = process.env.PORT || 4000;
 app.use(cors());
 app.use(express.json());
 
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if(!authorization) {
+    return res.status(401).send({error: true, message: 'Unauthorized access'});
+  }
+  //bearer token
+  const token = authorization.split(' ')[1];
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if(err) {
+      return res.status(401).send({error: true, message: 'Unauthorized access'});
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
+
 
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.d18ofon.mongodb.net/?retryWrites=true&w=majority`;
@@ -32,6 +48,12 @@ async function run() {
     const menuCollection = client.db("TastyTroveDb").collection("menu");
     const reviewCollection = client.db("TastyTroveDb").collection("reviews");
     const cartCollection = client.db("TastyTroveDb").collection("carts");
+
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token });
+    })
 
     //users
     app.get('/users', async (req, res) => {
@@ -82,11 +104,17 @@ async function run() {
     })
 
     // cart collection 
-    app.get('/carts', async (req, res) => {
+    app.get('/carts',verifyJWT, async (req, res) => {
       const email = req.query.email;
       if (!email) {
         res.send([]);
       }
+
+      const decodedEmail = req.decoded.email;
+      if(email !== decodedEmail) {
+        res.status(403).send({error: true, message: 'Provident access'});
+      }
+
       const query = { email: email };
       const result = await cartCollection.find(query).toArray();
       res.send(result);
