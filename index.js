b8d9a25cdbbc4bded014ed31035c906b8b01f9ac
selim121 <selimkhan.bdkhan@gmail.com>
2023-06-05 23:11:55 +0600

    admin home data get

--- index.js
+++ index.js
@@ -196,6 +196,19 @@ async function run() {
       res.send({insertResult, deleteResult});
     })
 
+    app.get('/admin-stats', verifyJWT, verifyAdmin, async(req, res) => {
+      const users = await usersCollection.estimatedDocumentCount();
+      const products = await menuCollection.estimatedDocumentCount();
+      const orders = await paymentCollection.estimatedDocumentCount();
+      const payments = await paymentCollection.find().toArray();
+      const revenue = payments.reduce((totalPayment, initialPayment) => totalPayment + initialPayment.price , 0)
+      res.send({
+        users,
+        products,
+        orders,
+        revenue
+      })
+    })
 
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
